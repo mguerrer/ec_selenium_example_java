0c2cc076aa9e435235036b8a9a2399652b2b02b3
mguerrero <marcosguerrerow@hotmail.com>
2021-06-02 19:56:18 -0400

    Ejemplo con menús.

--- src/kr/re/ec/seleniumexample/java/SeleniumExample.java
+++ src/kr/re/ec/seleniumexample/java/SeleniumExample.java
@@ -1,5 +1,6 @@
 package kr.re.ec.seleniumexample.java;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 
@@ -16,9 +17,17 @@ public class SeleniumExample {
 		
 		WebDriver driver = new InternetExplorerDriver(); 
 
-		driver.get("http://www.seoultech.ac.kr");
+		driver.get("http://webqamanh/wmweb/");
 		
+		driver.findElement(By.name("userID")).sendKeys("EB5235");
+		driver.findElement(By.name("userPassword")).sendKeys("PEPETAPIAD");
+		driver.findElement(By.name("Login")).click();
 		Thread.sleep(3000);
+		// Abre menú
+		driver.switchTo().frame("fraBottom");
+		driver.switchTo().frame("fraNavigator");
+		driver.findElement(By.xpath("//span[.='Mod Distribucion y Olas']")).click();
+		driver.findElement(By.xpath("//span[.='Mod Wave Mgmt Inquiries']")).click();
 		
 		driver.quit();
 	}
